199a2ae5c68f077fa9faf9e5850c77cc16bea209
mertkayaozkann <120733879+mertkayaozkann@users.noreply.github.com>
2023-05-25 23:37:10 +0300

    Add files via upload

---
+++ TDDExample/src/test/java/CalculatorTest.java
@@ -0,0 +1,69 @@
+//SWE202 SOFTWARE VERIFICATION AND VALIDATION
+// MERT KAYA ÖZKAN B201202037
+//Repoadress: https://github.com/mertkayaozkann/TDDexample
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    /*@Test
+    void testDivision1() {
+        assertEquals(5, Calculator.divide(10, 2));
+    }
+
+    @Test
+    void testDivision2() {
+        assertEquals(2.5, Calculator.divide(10, 4));
+    }
+
+    @Test
+    void testDivision3() {
+        assertEquals(2.5, Calculator.divide(12.5, 5));
+    }
+
+    @Test
+    void testDivision4() {
+        assertEquals(4, Calculator.divide(10, 2.5));
+    }
+
+    @Test
+    void testDivision5() {
+        assertEquals(5, Calculator.divide(12.5, 2.5));
+    }
+*/
+    @ParameterizedTest
+    @CsvSource({
+            "10, 2, 5",
+            "10, 4, 2.5",
+            "12.5, 5, 2.5",
+            "10, 2.5, 4",
+            "12.5, 2.5, 5"
+    })
+    void testDivision(double dividend, double divisor, double expected) {
+        assertEquals(expected, Calculator.divide(dividend, divisor));
+    }
+    @Test
+    void testDivision6() {
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(12.5, 0),
+                "IllegalArgumentException expected."
+        );
+
+    }
+}
